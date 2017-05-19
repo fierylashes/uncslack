f4505c61d242eed950c8be52a01a00843e4d8a7c
randomqwerty <dannyly@live.unc.edu>
2017-05-19 12:36:59 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -15,25 +15,17 @@ function createError(errorMessage) {
 }
 
 function getUsageHelp(commandName) {
-    function createSample(target) {
-        return commandName + ' *' + target + '* I know what you did last summer';
-    }
-
     var text = 'Expected usage: \n' +
-        commandName + ' help -- Displays help message.\n' +
-        createSample('@user') + ' -- Sends to the specified user.\n' +
-        createSample('#channel') + ' -- Sends to the specified public channel.\n' +
-        createSample('group') + ' -- Sends to the specified private group.\n' +
-        createSample(':here') + ' -- Sends to the current group/channel/DM where you type this command.';
-
+        commandName + ' *help* -- Displays help message.\n' +
+        commandName + ' [text here]';
     return text;
 }
 
 function getFullHelp(commandName) {
     var text =
-        'Allows to send anonymous messages to users, channels and groups.\n' +
-        'The most convenient and safe way is to open up a conversation with slackbot in Slack and type the commands there, so that nobody detects that you are typing and you don\'t accidentally reveal yourself by typing an invalid command.\n' +
-        'Messages and authors are not stored, and the sources are available at <https://github.com/TargetProcess/slack-anonymous>.\n' +
+        'Allows you to send anonymous messages to specified channels.\n' +
+        'The most convenient and safe way is to open up a conversation with Slackbot and type the commands there, so that nobody detects that you are typing and you don\'t accidentally reveal yourself by typing an invalid command.\n' +
+        'Messages and authors are not stored; the sources are available at <https://github.com/TargetProcess/slack-anonymous>.\n' +
         '\n' +
         getUsageHelp(commandName);
 
@@ -52,24 +44,9 @@ function createResponsePayload(requestBody) {
         return createError(getFullHelp(command));
     }
 
-    var splitted = text.split(" ");
-    if (splitted.length <= 1) {
-        return createError(getUsageHelp(command));
-    }
-
-    var target = splitted[0];
-    var remainingText = splitted.slice(1).join(' ');
-    remainingText = 'Someone said "' + remainingText + '"';
-
-    if (target === ':here') {
-        return {
-            channel: requestBody.channel_id,
-            text: remainingText
-        };
-    }
+    var remainingText = 'Someone said "' + text + '"';
 
     return {
-        channel: target,
         text: remainingText
     };
 }
@@ -86,9 +63,9 @@ app.post('/', function(req, response) {
         method: 'POST'
     }, function (error) {
         if(error) {
-            response.end('Unable to post your anonymous message: ' + JSON.stringify(error));
+            response.end('Unable to post your message: ' + JSON.stringify(error));
         } else {
-            response.end('Delivered! :cop:');
+            response.end('Delivered! :pepe:');
         }
 
     });
