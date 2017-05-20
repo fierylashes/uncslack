5f1bb0227e91907358484c2bb93e274abaeebf78
randomqwerty <dannyly@live.unc.edu>
2017-05-20 19:39:30 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ function getFullHelp(commandName) {
     var text =
         'Allows you to send anonymous messages to specified channels.\n' +
         'The most convenient and safe way is to open up a conversation with Slackbot and type the commands there, so that nobody detects that you are typing and you don\'t accidentally reveal yourself by typing an invalid command.\n' +
-        'Messages and authors are not stored; sources are available at <https://github.com/Randomqwerty/abcd>.\n' +
+        'Messages and authors are not stored; sources are available at <https://github.com/Randomqwerty/uncslack>.\n' +
         '\n' +
         getUsageHelp(commandName);
 
